20f2bd27037897b210243e5c9bfe50f14b93417f
def-my <define.oneself@gmail.com>
2013-07-26 05:25:15 +0900

    Fedora19用の設定ファイルを追加.

---
+++ Fedora19/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sudo sed -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
+
+sudo yum -y install gcc make automake autoconf libtool gcc-c++ kernel-headers-`uname -r` kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel nfs-utils bind-utils wget dkms curl bzip2
+
+sudo yum -y upgrade
+
+sudo yum -y update
+
+# Installing vagrant keys
+mkdir -pm 700 /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+cd /tmp
+sudo mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+sudo sh /mnt/VBoxLinuxAdditions.run
+sudo umount /mnt
+#rm -rf /home/vagrant/VBoxGuestAdditions*.iso
+
+sudo /etc/rc.d/init.d/vboxadd setup
