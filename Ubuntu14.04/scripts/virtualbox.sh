e83f13baeec9e6876d933d977c7c077346f862d0
geekduck <duck1218+github@gmail.com>
2014-09-06 23:30:52 +0900

    Add vmware.

--- Ubuntu14.04/scripts/virtualbox.sh
+++ Ubuntu14.04/scripts/virtualbox.sh
@@ -1,7 +1,1 @@
-apt-get -y install build-essential module-assistant
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf /home/vagrant/VBoxGuestAdditions*.iso
+apt-get -y install virtualbox-guest-utils
