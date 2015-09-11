e35fb519f826a1b4374a6c314dd70306d6acddc4
geekduck <duck1218+github@gmail.com>
2015-09-11 13:05:59 +0900

    Remove unnecessary package.

--- CentOS7/scripts/virtualbox.sh
+++ CentOS7/scripts/virtualbox.sh
@@ -4,4 +4,3 @@ mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions*.iso
-yum install -y virtualbox-guest-utils
