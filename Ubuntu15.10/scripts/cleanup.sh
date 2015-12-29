5897e4c870b283bb047b755da5910cd14a311300
geekduck <duck1218+github@gmail.com>
2015-12-29 17:08:42 +0900

    Modified.

--- Ubuntu15.10/scripts/cleanup.sh
+++ Ubuntu15.10/scripts/cleanup.sh
@@ -8,7 +8,7 @@ echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
 echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
+#rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+#rm /lib/udev/rules.d/75-persistent-net-generator.rules
