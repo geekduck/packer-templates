5070fa6b21e93e299a75304cb9e8ae9f8c0bb07d
geekduck <duck1218+github@gmail.com>
2016-04-22 08:00:27 +0900

    Update Ubuntu16.04

--- Ubuntu16.04/scripts/cleanup.sh
+++ Ubuntu16.04/scripts/cleanup.sh
@@ -7,8 +7,8 @@ rm -rf VBoxGuestAdditions*.iso VBoxGuestAdditions*.iso.?
 echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
-echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+#echo "cleaning up udev rules"
+#rm /etc/udev/rules.d/70-persistent-net.rules
+#mkdir /etc/udev/rules.d/70-persistent-net.rules
+#rm -rf /dev/.udev/
+#rm /lib/udev/rules.d/75-persistent-net-generator.rules
