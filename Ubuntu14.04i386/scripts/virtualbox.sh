f884d8c1401f009f7dcd89c2d99ebe6f4e41d01a
geekduck <duck1218+github@gmail.com>
2016-05-04 17:40:47 +0900

    Delete old os.

--- Ubuntu14.04i386/scripts/virtualbox.sh
+++
@@ -1,7 +0,0 @@
-apt-get -y install build-essential module-assistant
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf /home/vagrant/VBoxGuestAdditions*.iso
