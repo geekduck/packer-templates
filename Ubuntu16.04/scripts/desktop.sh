9b0c9b4bd55ed2636784d0eac03f73b698ac7867
geekduck <duck1218+github@gmail.com>
2016-05-07 23:21:19 +0900

    Add ubuntu desktop template.

---
+++ Ubuntu16.04/scripts/desktop.sh
@@ -0,0 +1,15 @@
+apt-get install -y ubuntu-desktop
+
+USERNAME=vagrant
+LIGHTDM_CONFIG=/etc/lightdm/lightdm.conf
+GDM_CUSTOM_CONFIG=/etc/gdm/custom.conf
+
+mkdir -p $(dirname ${GDM_CUSTOM_CONFIG})
+echo "[daemon]" >> $GDM_CUSTOM_CONFIG
+echo "# Enabling automatic login" >> $GDM_CUSTOM_CONFIG
+echo "AutomaticLoginEnable=True" >> $GDM_CUSTOM_CONFIG
+echo "AutomaticLoginEnable=${USERNAME}" >> $GDM_CUSTOM_CONFIG
+
+echo "==> Configuring lightdm autologin"
+echo "[SeatDefaults]" >> $LIGHTDM_CONFIG
+echo "autologin-user=${USERNAME}" >> $LIGHTDM_CONFIG
