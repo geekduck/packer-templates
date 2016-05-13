677b1818924d5a887737e1046be114d31f7fd459
geekduck <duck1218+github@gmail.com>
2016-05-13 14:05:27 +0900

    Fix clean script.

---
+++ CentOS6x86_64/scripts/cleanup-desktop.sh
@@ -0,0 +1,8 @@
+yum -y clean all
+rm -rf VBoxGuestAdditions_*.iso
+rm -rf /tmp/rubygems-*
+
+# clean up redhat interface persistence
+sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
+sudo sed -i 's/^HWADDR.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
+sudo sed -i 's/^UUID.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
--- CentOS6x86_64/scripts/desktop.sh
+++ CentOS6x86_64/scripts/desktop.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-cat /etc/redhat-release
-if grep -q -i "release 7" /etc/redhat-release ; then
-    # set the graphical.target for systemd so it will run startx on boot
-    sudo ln -sf /lib/systemd/system/graphical.target /etc/systemd/system/default.target
-fi
-
 # Configure gdm autologin.
 GDM_CONFIG=/etc/gdm/custom.conf.rpmsave
-
 if [ -f $GDM_CONFIG ]; then
     sudo sed -i s/"daemon]$"/"daemon]\nAutomaticLoginEnable=true\nAutomaticLogin=vagrant"/ /etc/gdm/custom.conf
 fi
