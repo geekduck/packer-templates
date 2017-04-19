82ef326ef634b6968537a544e1fcc30607d8dcc5
geekduck <duck1218+github@gmail.com>
2017-04-20 00:42:40 +0900

    Fix mount directory

--- Ubuntu16.10/scripts/virtualbox.sh
+++ Ubuntu16.10/scripts/virtualbox.sh
@@ -5,3 +5,8 @@ mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions*.iso
+
+# VirtualBox 5.1.20
+if [ ! -f /sbin/mount.vboxsf ] && [ -f /usr/lib/x86_64-linux-gnu/VBoxGuestAdditions/mount.vboxsf ]; then
+    ln -sf /usr/lib/x86_64-linux-gnu/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf
+fi
