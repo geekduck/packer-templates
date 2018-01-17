f42561741f59347c02813b5f82c633a6dbb6fe3d
geekduck <duck1218+github@gmail.com>
2018-01-17 23:30:10 +0900

    Delete CentOS6

--- CentOS6x86_64/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y clean all
-rm -rf VBoxGuestAdditions_*.iso
-rm -rf /tmp/rubygems-*
-
-# clean up redhat interface persistence
-sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
-sudo sed -i 's/^HWADDR.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i 's/^UUID.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
--- CentOS6x86_64/scripts/virtualbox.sh
+++
@@ -1,6 +0,0 @@
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-cd /tmp
-mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf /home/vagrant/VBoxGuestAdditions*.iso
