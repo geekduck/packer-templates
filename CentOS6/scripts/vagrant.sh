856e907b2aae8925fb562e4398a03171c7b3eb96
geekduck <duck1218+github@gmail.com>
2020-12-23 18:11:43 +0900

    Delete CentOS6

--- CentOS6/scripts/vagrant.sh
+++
@@ -1,8 +0,0 @@
-date > /etc/vagrant_box_build_time
-
-mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
-    -O /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
