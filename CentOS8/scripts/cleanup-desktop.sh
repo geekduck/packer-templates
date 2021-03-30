590b52c2a96f3a59f23f2f7a9b77026646988591
geekduck <duck1218+github@gmail.com>
2021-03-30 12:33:22 +0900

    Delete CentOS8

--- CentOS8/scripts/cleanup-desktop.sh
+++
@@ -1,9 +0,0 @@
-yum -y clean all
-rm -rf VBoxGuestAdditions_*.iso
-rm -rf /tmp/rubygems-*
-
-# clean up redhat interface persistence
-sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
-ETHDEV=`ls /etc/sysconfig/network-scripts/ifcfg-* | grep -v lo`
-sudo sed -i 's/^HWADDR.*$//' $ETHDEV
-sudo sed -i 's/^UUID.*$//' $ETHDEV
